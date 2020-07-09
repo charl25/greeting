2f878552047ab2f2648c9624be3d091e7ff013ee
charl25 <43599003+charl25@users.noreply.github.com>
2020-07-09 20:33:34 +0200

    first error fix

--- greet.js
+++ greet.js
@@ -6,35 +6,37 @@ const clearBtn = document.querySelector(".clearBtn")
 
 var names=[];
 
-var namesLS = localStorage['namesGreeted'];
-
 const greetRadio = GreetRadio();
 
-greetButton.addEventListener('click', function(e){
+greetTxt.innerHTML = greetRadio.greetName();
+counter.innerHTML = JSON.parse(localStorage['count']);    
 
+greetButton.addEventListener('click', function(e){
+  
   var langChecked = document.querySelector("input[name='language']:checked");
   if(langChecked){
     var checkedLang = langChecked.value;
-    }
+  }
   var person = name.value.trim();
-
+if(/[a-zA-z]$/.test(person) && !(checkedLang === undefined)){
+  
   greetRadio.setLanguage(checkedLang);
   greetRadio.setName(person);
   greetRadio.greet();
-
-  greetTxt.innerHTML = greetRadio.greetName();
-  counter.innerHTML = greetRadio.counter();
-
+  
   localStorage['count']= greetRadio.counter();
-    
+  
   localStorage['namesGreeted'] = JSON.stringify(greetRadio.list());
-    
+  
+  greetTxt.innerHTML = greetRadio.greetName();
+  counter.innerHTML = JSON.parse(localStorage['count']);    
+}
 })
 
 clearBtn.addEventListener('click', function(e){
   greetRadio.clear();
   localStorage['namesGreeted'] = JSON.stringify(greetRadio.list());
   localStorage['count']= greetRadio.counter();
-
+  
   location.reload ();
 })
